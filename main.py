05c08329ea3f1ea9e49d88a6d96d3f2f3c654955
paidforby <ggallo102@gmail.com>
2018-10-14 09:17:30 -0700

    testing, not sure what changed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 
-cv2.namedWindow("preview")
-cv2.namedWindow("animate")
+#cv2.namedWindow("preview")
+#cv2.namedWindow("animate")
 vc = cv2.VideoCapture(0)
 
 if vc.isOpened(): # try to get the first frame
