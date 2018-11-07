216c9927a76c965e736ba1c0e62ced2891ded697
paidforby <ggallo102@gmail.com>
2018-11-06 17:44:07 -0800

    python script is now usable

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # stop-mo-inspo
 inspiration for stop motion projects
 
-This is a very rudimentary web app for creating stop motion videos. It does (or will do) the basics; show your current web cam feed, capture a frame from web cam by pressing space, show a timeline of previously captured frames, and show a looped "video" of captured frames. Hoping to add onion skinning (i.e. overlays of previous frames on current feed), more control options for playback, and some export options (ordered pics, full videos, self-contained gifs, etc).
+This is now a python program for creating stop motion videos.   
+Currently, it does the basics; 
+- shows your current web cam feed or USB camera feed (set by changing `camera = cv2.VideoCapture(0)` to either 0 or 1) 
+- captures a frame from camera by pressing spacebar, 
+- shows a looped video of captured frames. 
+- onion skinning (i.e. overlays of previous frames on current feed)
+- saves captured frames in `frames` directory 
 
 To get started, open terminal and execute the following commands:
 
 1. Clone this repo with ```git clone https://github.com/paidforby/stop-mo-inspo``` 
-2. Move into the repo with ```cd p5-starter``` or copy the contents in to a different folder
-3. Create local copies of the p5 libraries with ```./requirments.sh```
-3. Start a webserver with ```python -m SimpleHTTPServer 8000``` or another preferred webserver
-4. In a web browser, open http://localhost:8000
-5. Capture frames by pressing (or holding) space bar.
+2. Move into the repo with ```cd stop-mo-inspo``` or copy the contents in to a different folder
+2. Install dependencies for python OpenCV (will add instructions or requirements.txt)
+4. Run `python ./main.py`
 
-Now start messing with the ```main.js``` file to modify the script being run.
+Once you are happy with your animation, you can create a video from your captured frames by executing the following ffmpeg command within the frames directory,
+```
+ffmpeg -r 6 -i frame%04d.jpg -vcodec libx264 -crf 25 -pix_fmt yuv420p horse_in_motion.mp4
+```
+where `-r 6` specifies the frame rate or your animation
+
+It is also great idea to backup the frames in a new folder by running something like,
+```
+cp -r frames horses_in_moition
+```
+
+Now start messing with the ```main.py``` file to modify the script being run.
--- main.py
+++ main.py
@@ -1,31 +1,85 @@
+# import the necessary packages
+from __future__ import print_function
+#import numpy as np
+import time
 import cv2
+ 
+cv2.namedWindow("preview")
+cv2.namedWindow("animate")
+camera = cv2.VideoCapture(1)
 
-#cv2.namedWindow("preview")
-#cv2.namedWindow("animate")
-vc = cv2.VideoCapture(0)
+framerate = .0416 #float(1/24)
+stillnumber = 4 
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
+lastFrameTime = time.time()
+ 
+print("Hey there ")
+
+def callback(x):
+    #a = cv2.getTrackbarPos("alpha", "preview")
+    #print("alpha={}".format(a))
+    pass
+
+# Add a slider
+cv2.createTrackbar("a", "preview", 0, 100, callback)
+
+if camera.isOpened(): # try to get the first frame
+    rval, frame = camera.read()
     count = 0
     order = 0 
+    still = 0
 else:
     rval = False
 
+print(time.time() - lastFrameTime)
+
 while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
+    #alpha = .5
+    rval, frame = camera.read()
     key = cv2.waitKey(20)
+
+    #overlay = frame.copy()
+    output = frame.copy()
+
+    alpha = float(cv2.getTrackbarPos("a", "preview"))
+    alpha = alpha/100
+
+    if count > 0:
+        previous_frame = "frames/frame" + str('{:04d}'.format(count-1)) + ".jpg"
+        overlay = cv2.imread(previous_frame)
+        #cv2.rectangle(overlay, (420, 205), (595, 385), (0, 0, 255), -1)
+        #cv2.putText(overlay, "PyImageSearch: alpha={}".format(alpha), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
+        cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+    
+    if count > 5:
+        if (time.time() - lastFrameTime) > framerate:
+            framename = "frames/frame" + str('{:04d}'.format(order)) + ".jpg"
+            img = cv2.imread(framename,1)
+            cv2.imshow("animate", img) #display image
+            still = still + 1
+            if still > stillnumber:
+                order = order + 1
+                still = 0
+            if order >= count:
+                order = 0
+            lastFrameTime = time.time()
+
+    cv2.imshow("preview", output)
+
     if key == 32: # capture on SPACE 
-        filename = "frames/frame" + str(count) + ".jpg"
+        filename = "frames/frame" + str('{:04d}'.format(count)) + ".jpg"
         cv2.imwrite(filename, frame) #save image
         count = count + 1
-    if key == 13: # hold ENTER to playback frames 
-        framename = "frames/frame" + str(order) + ".jpg"
-        img = cv2.imread(framename,1)
-        cv2.imshow("animate", img) #display image
-        order = order + 1
-        if order >= count:
-            order = 0
+
+    #if key == 13: # hold ENTER to playback frames 
+    #    framename = "frames/frame" + str('{:04d}'.format(order)) + ".jpg"
+    #    img = cv2.imread(framename,1)
+    #    cv2.imshow("animate", img) #display image
+    #    order = order + 1
+    #    if order >= count:
+    #        order = 0
+
     if key == 27: # exit on ESC
         break
+
 cv2.destroyWindow("preview")
